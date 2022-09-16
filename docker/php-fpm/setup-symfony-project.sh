cd8d9b62fe0eef0b4b139b8190e659faf839756b
Caio Gallo <caiog@upoutsourcing.com>
2022-09-15 23:00:45 -0300

    Remove untracked files

--- docker/php-fpm/setup-symfony-project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git config --global user.email "dsad@fwef.ggre"
-git config --global user.name "gregre"
-symfony new /var/www --version="6.1.*" --webapp
-composer i -o;
-wait-for-it db:5432 -- bin/console doctrine:migrations:migrate
-cp /home/.htaccess /var/www/.htaccess
